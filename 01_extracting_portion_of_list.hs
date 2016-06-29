eab87800f468aac7bab3c13e88d1919b138c407b
Attila Domokos <adomokos@gmail.com>
2016-06-29 07:39:28 -0500

    Fix the first kata

--- 01_extracting_portion_of_list.hs
+++ 01_extracting_portion_of_list.hs
@@ -4,24 +4,21 @@ import Control.Exception (evaluate)
 
 main :: IO ()
 main = hspec $ do
-    describe "Extracting portion of list" $
-        it "takes elements from a list" $
-            ___ ['a'..'z'] `shouldBe` "abcdefg"
-            ___ [1..10] `shouldBe` [1,2,3]
-            ___ [] `shouldBe` []
-            ___ (enumFrom 10) `shouldBe` [10,11,12,13,14]
-        it "can drop elements from a list" $
-            ___ [1..10] `shouldBe` [6,7,8,9,10]
-            ___ ['a'..'g'] `shouldBe` "defg"
-            ___ (enumFromTo 10 20) `shouldBe` [16,18,20]
-            ___ [] `shouldBe` []
-        it "can split a collection" $
-            ___ [1..10] `shouldBe` ([1,2,3,4,5],[6,7,8,9,10])
-            ___ [] `shouldBe` ([],[])
-        it "can take while" $
-            ___ [1..10] `shouldBe` [1,2]
-            ___ (enumFromTo 5 15) `shouldBe` [5,6,7]
-            ___ "abracadabra" `shouldBe` "a"
-        it "can drop while" $
-            ___ [1..10] `shouldBe` [5,6,7,8,9,10]
-            ___ "abracadabra" `shouldBe` "bracadabra"
+    describe "Extracting portion of list" $ do
+        it "takes elements from a list" $ do
+            take 7 ['a'..'z'] `shouldBe` "abcdefg"
+            {- [1..10] `shouldBe` [1,2,3] -}
+            {- (enumFrom 10) `shouldBe` [10,11,12,13,14] -}
+        {- it "can drop elements from a list" $ do -}
+            {- [1..10] `shouldBe` [6,7,8,9,10] -}
+            {- ['a'..'g'] `shouldBe` "defg" -}
+            {- (enumFromTo 10 20) `shouldBe` [16,17,18,19,20] -}
+        {- it "can split a collection" $ do -}
+            {- [1..10] `shouldBe` ([1,2,3,4,5],[6,7,8,9,10]) -}
+        {- it "can take while" $ do -}
+            {- [1..10] `shouldBe` [1,2] -}
+            {- (enumFromTo 5 15) `shouldBe` [5,6,7] -}
+            {- "abracadabra" `shouldBe` "a" -}
+        {- it "can drop while" $ do -}
+            {- [1..10] `shouldBe` [5,6,7,8,9,10] -}
+            {- "abracadabra" `shouldBe` "bracadabra" -}
