9108718b4f0a0a69f168565d999a09977054bf8d
Attila Domokos <adomokos@gmail.com>
2016-09-18 11:19:14 -0500

    Learn about types

--- test/05_types_Spec.hs
+++ test/05_types_Spec.hs
@@ -28,5 +28,14 @@ main = hspec $ do
             read "8.2" + 3.8 `shouldBe` 12
             (read "[1,2,3,4]" :: [Int]) `shouldBe` [1,2,3,4]
             (read "(3, 'a')" :: (Int, Char)) `shouldBe` (3, 'a')
-
-
+        it "can provide ranges, next items for Enum types" $ do
+            ['a'..'e'] `shouldBe` "abcde"
+            [LT .. GT] `shouldBe` [LT,EQ,GT]
+            [3.. 5] `shouldBe` [3,4,5]
+            succ 'B' `shouldBe` 'C'
+    describe "Num is a numeric typeclass" $ do
+        it "can act like numbers" $ do
+            (20 :: Int) `shouldBe` 20
+    describe "fromIntegral is there historical reasons" $ do
+        it "can add Int and Floating point numbers" $ do
+            fromIntegral(length [1,2,3,4]) + 3.2 `shouldBe` 7.2
