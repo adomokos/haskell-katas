2a1f5de492378937ebdd6c2dd365f249df4e9450
Attila Domokos <adomokos@gmail.com>
2016-09-19 21:11:08 -0500

    Set up template for exercises

--- test/02_list_comprehensions.hs
+++ test/02_list_comprehensions.hs
@@ -5,20 +5,26 @@ import Control.Exception (evaluate)
 main :: IO ()
 main = hspec $ do
     describe "List Comprehensions" $ do
-        it "squares numbers" $ do
-            [x^2 | x <- [1..5]] `shouldBe` [1,4,9,16,25]
-        it "can use predicates" $ do
-            [x^2 | x <- [1..10], rem x 2 == 0] `shouldBe` [4,16,36,64,100]
-        it "can exhaust the lists" $ do
-            [x^y | x <- [1..5], y <- [2,3]] `shouldBe` [1,1,4,8,9,27,16,64,25,125]
-        it "can exhaust the lists and take predicate" $ do
-            [x^y | x <- [1..5], y <- [2,3], x^y < 50] `shouldBe` [1,1,4,8,9,27,16,25]
+        it "squares numbers from 1-5" $ do
+            pending
+            [] `shouldBe` [1,4,9,16,25]
+            {- The solution is: [x^2 | x <- [1..5]]  -}
+        it "squares numbers from 1-5 and filters for even" $ do
+            pending
+            [] `shouldBe` [4,16]
+        it "can raise the numbers from 1-5 to the power of 2 and 3" $ do
+            pending
+            [] `shouldBe` [1,1,4,8,9,27,16,64,25,125]
+        it "can filter the previous list to numbers below 50" $ do
+            pending
+            [] `shouldBe` [1,1,4,8,9,27,16,25]
         it "can create tuples from lists" $ do
-            [(x, y) | x <- [1,2,3], y <- [6, 7]] `shouldBe` [(1,6),(1,7),(2,6),(2,7),(3,6),(3,7)]
-            [(x, y) | x <- [1,2,3], y <- ['a', 'b']] `shouldBe` [(1,'a'),(1,'b'),(2,'a'),(2,'b'),(3,'a'),(3,'b')]
-        it "finds upper-case letters" $ do
-            [x | x <- "Three Letter Acronym", elem x ['A'..'Z']] `shouldBe` "TLA"
+            pending
+            [] `shouldBe` [(1,'a'),(1,'b'),(2,'a'),(2,'b'),(3,'a'),(3,'b')]
+        it "can filter for upper-case letters" $ do
+            pending
+            [] `shouldBe` "ABC"
         it "can be generalized into a function" $ do
-            let f xs = [x | x <- xs, elem x ['A'..'Z']]
-            f "Self Contained Underwater Breating Apparatus" `shouldBe` "SCUBA"
-            f "National Aeronautics and Space Administration" `shouldBe` "NASA"
+            pending
+            let f xs = []
+            f "Apple Bick Cat" `shouldBe` "ABC"
