8725b81cf1d3015de90add373a074f15e4e5b407
Attila Domokos <adomokos@gmail.com>
2016-10-11 18:05:44 -0500

    Implement basic functions with folds

--- test/14_folds.hs
+++ test/14_folds.hs
@@ -14,6 +14,24 @@ map' :: (a -> b) ->[a] -> [b]
 {- map' f xs = foldr (\x acc -> f x : acc) [] xs -}
 map' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: (Ord a) => [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f = foldr (\x acc -> if f x then x : acc else acc) []
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
 main :: IO()
 main = hspec $ do
     describe "Folds" $ do
@@ -24,3 +42,15 @@ main = hspec $ do
             elem' 5 [1,2,3,4] `shouldBe` False
         it "can map over values with foldr" $ do
             map' (*2) [1,2,3] `shouldBe` [2,4,6]
+        it "can find the maximum in a list" $ do
+            maximum' [1,2,3] `shouldBe` 3
+        it "can reverse a list with foldl" $ do
+            reverse' [1,3,5] `shouldBe` [5,3,1]
+        it "can calculate a product of a list" $ do
+            product' [2,2,3] `shouldBe` 12
+        it "can filter a list with foldr" $ do
+            filter' (<3) [1,2,3,5] `shouldBe` [1,2]
+        it "can pick the head of the list" $ do
+            head' [5,4,3] `shouldBe` 5
+        it "can pick the last item of the list" $ do
+            last' [5,4,3] `shouldBe` 3
