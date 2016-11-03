2fd2c372080338069cea78f48bb0821766ec26bf
Attila Domokos <adomokos@gmail.com>
2016-11-03 08:55:50 -0500

    Learning more about list functions

--- test/19_more_list_functions.hs
+++ test/19_more_list_functions.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 import Test.QuickCheck
 import Control.Exception (evaluate)
 import qualified Data.List as L
+import Data.Function
 
 search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
@@ -28,3 +29,42 @@ main = hspec $ do
             L.tails "woot" `shouldBe` ["woot", "oot", "ot", "t", ""]
         it "can use foldl with tails to search for item in a list" $ do
             search "cat" "im a cat burglar" `shouldBe` True
+        it "can tell if a string starts with another one" $ do
+            "hey" `L.isPrefixOf` "hey there!" `shouldBe` True
+            "hey" `L.isPrefixOf` "oh, hey there!" `shouldBe` False
+        it "can tell if a string ends with another one" $ do
+            "there!" `L.isSuffixOf` "oh, hey there!" `shouldBe` True
+            "there!" `L.isSuffixOf` "oh, hey there" `shouldBe` False
+        it "can partition lists based ona predicate" $ do
+            L.partition (>3) [1,3,5,6,3,2,1,0,3,7]
+                `shouldBe` ([5,6,7],[1,3,3,2,1,0,3])
+        it "finds the first match in a list" $ do
+            L.find (>4) [1,2,3,4,5,6] `shouldBe` Just 5
+            L.find (>7) [1,2,3,4,5,6] `shouldBe` Nothing
+        it "finds the index of an element in a list" $ do
+            L.elemIndex 4 [1,2,3,4,5,6] `shouldBe` Just 3
+            L.elemIndex 10 [1,2,3,4,5,6] `shouldBe` Nothing
+        it "finds the index of a predicate" $ do
+            L.findIndex (==4) [5,3,2,1,6,4] `shouldBe` Just 5
+        it "can zip more than 2 lists" $ do
+            L.zipWith3 (\x y z -> x + y + z) [1,2,3] [4,5,2,2] [2,2,3]
+                `shouldBe` [7,9,8]
+        it "creates a string array by line breaks" $ do
+            L.lines "first line\nsecond line\nthird line"
+                `shouldBe` ["first line","second line", "third line"]
+        it "creates a string from a string array" $ do
+            L.unlines ["first line","second line","third line"]
+                `shouldBe` "first line\nsecond line\nthird line\n"
+        it "breaks a string apart into words" $ do
+            L.words "hey   these are the  words"
+                `shouldBe` ["hey","these","are","the","words"]
+        it "can compact a list" $ do
+            L.nub [1,2,3,4,1,2,3,4,1,2,3,4] `shouldBe` [1,2,3,4]
+        it "combines two lists, removing duplicates" $ do
+            L.union [1..7] [5..10] `shouldBe` [1,2,3,4,5,6,7,8,9,10]
+        it "finds the same items in two lists" $ do
+            L.intersect [1..7] [5..10] `shouldBe` [5,6,7]
+        it "can sort a list of lists by their lengths" $ do
+            let xs = [[5,4,5,4,4],[1,2,3],[3,5,4,3],[],[2],[2,2]]
+            L.sortBy (compare `on` length) xs
+                `shouldBe` [[],[2],[2,2],[1,2,3],[3,5,4,3],[5,4,5,4,4]]
