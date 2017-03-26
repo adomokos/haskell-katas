9ffd79071853db31725ad616c2fad73f46fef9a5
Attila Domokos <adomokos@gmail.com>
2017-03-26 09:22:30 -0500

    Add more Char examples

--- test/20_char_functions.hs
+++ test/20_char_functions.hs
@@ -8,9 +8,25 @@ import Data.Function
 main :: IO()
 main = hspec $ do
     describe "Char functions" $ do
+        it "can check if string isAlphaNum" $ do
+            all C.isAlphaNum "bobby283" `shouldBe` True
+            all C.isAlphaNum "some\n" `shouldBe` False
         it "can use isSpace to simulate List's words function" $ do
+            {- use groupBy with C.isSpace -}
             L.groupBy ((==) `on` C.isSpace) "hey guys it's me"
                 `shouldBe` ["hey"," ","guys"," ","it's"," ","me"]
         it "can filter out spaces from the previous example" $ do
+            {- remove the spaces  -}
             (L.filter (not . any C.isSpace) . L.groupBy ((==) `on` C.isSpace) $ "hey guys it's me")
                 `shouldBe` ["hey","guys","it's","me"]
+        it "can categorize characters" $ do
+            C.generalCategory ' ' `shouldBe` C.Space
+            C.generalCategory 'U' `shouldBe` C.UppercaseLetter
+            C.generalCategory 'z' `shouldBe` C.LowercaseLetter
+            map C.generalCategory " \t\nA9"
+                `shouldBe` [C.Space,C.Control,C.Control,C.UppercaseLetter,C.DecimalNumber]
+        it "'ord' and 'chr' convert chars back and forth" $ do
+            C.ord 'a' `shouldBe` 97
+            C.chr 97 `shouldBe` 'a'
+        it "can encode a string by shifting its value with provided num" $ do
+            (decode 3 $ encode 3 "Heeeeey") `shouldBe` "Heeeeey"
