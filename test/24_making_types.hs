06c6681699d5020d4f9890f247996f3631d2a94d
Attila Domokos <adomokos@gmail.com>
2017-03-29 08:54:57 -0500

    Nudge a shape - and create it with base functions

--- test/24_making_types.hs
+++ test/24_making_types.hs
@@ -17,6 +17,19 @@ surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) =
     (abs $ x2 - x1) * (abs $ y2 - y1)
 
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b =
+    Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b =
+    Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRectangle :: Float -> Float -> Shape
+baseRectangle width height =
+    Rectangle (Point 0 0) (Point width height)
+
 main :: IO()
 main = hspec $ do
     describe "Custom Types" $ do
@@ -33,3 +46,13 @@ main = hspec $ do
             (surface $ Circle (Point 10 20) 10) `shouldBe` 314.15927
             (surface $ Rectangle (Point 0 0) (Point 100 100))
                 `shouldBe` 10000.0
+        it "can nudge a Shape, still holding its dimension" $ do
+           (show $ (nudge (Circle (Point 0 0) 3) 2 4))
+                `shouldBe` "Circle (Point 2.0 4.0) 3.0"
+           (show $ (nudge (Rectangle (Point 0 0) (Point 4 4)) 2 4))
+                `shouldBe` "Rectangle (Point 2.0 4.0) (Point 6.0 8.0)"
+        it "can nudge a Shape starting with base shapes" $ do
+           (show $ (nudge (baseCircle 3) 2 4))
+                `shouldBe` "Circle (Point 2.0 4.0) 3.0"
+           (show $ (nudge (baseRectangle 4 4)) 2 4)
+                `shouldBe` "Rectangle (Point 2.0 4.0) (Point 6.0 8.0)"
