af9972117900e3f357db6910c287b99d368a4946
Attila Domokos <adomokos@gmail.com>
2017-03-29 19:45:05 -0500

    Make custom types Equatable for comparison

--- test/24_making_types.hs
+++ test/24_making_types.hs
@@ -9,8 +9,8 @@ import Control.Exception (evaluate)
 {- surface (Circle _ _ r) = pi * r ^ 2 -}
 {- surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1) -}
 
-data Point = Point Float Float deriving (Show)
-data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+data Point = Point Float Float deriving (Show, Eq)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show, Eq)
 
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
@@ -38,21 +38,21 @@ main = hspec $ do
             {- (surface $ Rectangle 0 0 100 100) -}
                 {- `shouldBe` 10000.0 -}
         {- it "can print out the Shape by adding Show" $ do -}
-            {- (show $ Circle 10 20 5) `shouldBe` "Circle 10.0 20.0 5.0" -}
+            {- Circle 10 20 5 `shouldBe` Circle 10.0 20.0 5.0 -}
         {- it "can use Value constructors as functions" $ do -}
-            {- (map show $ map (Circle 10 20) [4,5]) -}
-                {- `shouldBe` ["Circle 10.0 20.0 4.0", "Circle 10.0 20.0 5.0"] -}
+            {- map (Circle 10 20) [4,5] -}
+                {- `shouldBe` [Circle 10.0 20.0 4.0, Circle 10.0 20.0 5.0] -}
         it "can calculate surface for two types with Points" $ do
             (surface $ Circle (Point 10 20) 10) `shouldBe` 314.15927
             (surface $ Rectangle (Point 0 0) (Point 100 100))
                 `shouldBe` 10000.0
         it "can nudge a Shape, still holding its dimension" $ do
-           (show $ (nudge (Circle (Point 0 0) 3) 2 4))
-                `shouldBe` "Circle (Point 2.0 4.0) 3.0"
-           (show $ (nudge (Rectangle (Point 0 0) (Point 4 4)) 2 4))
-                `shouldBe` "Rectangle (Point 2.0 4.0) (Point 6.0 8.0)"
+           (nudge (Circle (Point 0 0) 3) 2 4)
+                `shouldBe` Circle (Point 2.0 4.0) 3.0
+           (nudge (Rectangle (Point 0 0) (Point 4 4)) 2 4)
+                `shouldBe` Rectangle (Point 2.0 4.0) (Point 6.0 8.0)
         it "can nudge a Shape starting with base shapes" $ do
-           (show $ (nudge (baseCircle 3) 2 4))
-                `shouldBe` "Circle (Point 2.0 4.0) 3.0"
-           (show $ (nudge (baseRectangle 4 4)) 2 4)
-                `shouldBe` "Rectangle (Point 2.0 4.0) (Point 6.0 8.0)"
+           (nudge (baseCircle 3) 2 4)
+                `shouldBe` Circle (Point 2.0 4.0) 3.0
+           (nudge (baseRectangle 4 4)) 2 4
+                `shouldBe` Rectangle (Point 2.0 4.0) (Point 6.0 8.0)
