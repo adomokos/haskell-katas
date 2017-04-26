d3b5633b6e4f34b30b03f9e577736f9e79e8813d
Attila Domokos <adomokos@gmail.com>
2017-04-26 10:07:06 -0500

    Learn about IO ()

---
+++ bin/helloworld.hs
@@ -0,0 +1,62 @@
+{- main = putStrLn "hello, world" -}
+
+{-
+greet :: String -> String
+greet name = "Hello, " ++ name ++ "!"
+
+main = do
+    putStrLn "Hello, what's your name?"
+    name <- getLine
+    putStrLn (greet name)
+-}
+
+{-
+{- Using let bindings -}
+import Data.Char
+
+main = do
+    putStrLn "What's your first name?"
+    firstName <- getLine
+    putStrLn "What's your last name?"
+    lastName <- getLine
+    let bigFirstName = map toUpper firstName
+        bigLastName = map toUpper lastName
+    putStrLn $ "hey " ++ bigFirstName ++ " " ++
+                bigLastName ++ ", how are you?"
+-}
+
+{-
+{- return -}
+reverseWords :: String -> String
+reverseWords = unwords . map reverse . words
+
+main = do
+    putStrLn "Type a word or two"
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn $ reverseWords line
+            main
+-}
+
+{-
+{- return result is thrown away -}
+main = do
+    return ()
+    return "HAHAHA"
+    line <- getLine
+    return "BLAH BLAH BLAH"
+    return 4
+    putStrLn line
+-}
+
+{-
+{- return's result is captured -}
+main = do
+    a <- return "hell"
+    b <- return "yeah!"
+    putStrLn $ a ++ " " ++ b
+-}
+
+
