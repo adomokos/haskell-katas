b0a473a72b291cbfebeb70ac365c190210cf9bba
Attila Domokos <adomokos@gmail.com>
2017-05-09 08:47:51 -0500

    Copy files lazy, in 64Kb chunks

---
+++ test/io/25_copy_file.hs
@@ -0,0 +1,14 @@
+{-
+    Copies one file into the other
+-}
+import System.Environment
+import qualified Data.ByteString.Lazy as B
+
+main = do
+    (fileName1:fileName2:_) <- getArgs
+    copyFile fileName1 fileName2
+
+copyFile :: FilePath -> FilePath -> IO ()
+copyFile source dest = do
+    contents <- B.readFile source
+    B.writeFile dest contents
