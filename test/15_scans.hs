1f0d530ea90688da130d6a389d6d5726211a6926
Attila Domokos <adomokos@gmail.com>
2017-07-20 15:43:35 +0200

    Add an example for scanl to keep only if larger than next

--- test/15_scans.hs
+++ test/15_scans.hs
@@ -6,5 +6,7 @@ main :: IO()
 main = hspec $ do
     describe "Scans are like folds" $ do
         it "they are reporting intermediate results" $ do
-           scanl (+) 0 [1,2,3] `shouldBe` [0,1,3,6]
-           scanr (+) 0 [1,2,3] `shouldBe` [6,5,3,0]
+            scanl (+) 0 [1,2,3] `shouldBe` [0,1,3,6]
+            scanr (+) 0 [1,2,3] `shouldBe` [6,5,3,0]
+            scanl1 (\x acc -> if x > acc then x else acc) [3,4,5,3,7,9,2,1]
+                `shouldBe` [3,4,5,5,7,9,9,9]
