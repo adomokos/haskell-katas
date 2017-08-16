8e567c01bf0e46112549bf8e4ea0e21b53f98be9
Attila Domokos <adomokos@gmail.com>
2017-08-16 16:59:10 -0500

    Create the intro Monoid example

--- test/40_monoids.hs
+++ test/40_monoids.hs
@@ -29,29 +29,33 @@ main :: IO ()
 main = hspec $ do
     describe "Monoids" $ do
         it "defines mappend and mempty" $ do
-            [1,2,3] `mappend` [4,5,6]
-                `shouldBe` [1,2,3,4,5,6]
-            ("one" `mappend` "two") `mappend` "tree"
-                `shouldBe` "onetwotree"
-            "one" `mappend` ("two" `mappend` "tree")
-                `shouldBe` "onetwotree"
-            "one" `mappend` "two" `mappend` "tree"
-                `shouldBe` "onetwotree"
-            "pang" `mappend` mempty `shouldBe` "pang"
-            mconcat [[1,2],[3,6],[9]]
-                `shouldBe` [1,2,3,6,9]
-            (mempty :: [Int]) `shouldBe` []
+            pending
+            {- [1,2,3] ___ [4,5,6] -}
+                {- `shouldBe` [1,2,3,4,5,6] -}
+            {- ("one" ___ "two") ___ "tree" -}
+                {- `shouldBe` "onetwotree" -}
+            {- "one" ___ ("two" ___ "tree") -}
+                {- `shouldBe` "onetwotree" -}
+            {- "one" ___ "two" `mappend` ___ -}
+                {- `shouldBe` "onetwotree" -}
+            {- "pang" `mappend` mempty `shouldBe` "pang" -}
+            {- ___ [[1,2],[3,6],[9]] -}
+                {- `shouldBe` [1,2,3,6,9] -}
+            {- (___ :: [Int]) `shouldBe` [] -}
         it "defines Product" $ do
-            (getProduct $ Product 3 `mappend` Product 9)
-                `shouldBe` 27
-            (getProduct $ Product 3 `mappend` mempty)
-                `shouldBe` 3
-            (getProduct . mconcat . map Product $ [3,4,2])
-                `shouldBe` 24
+            pending
+            {- (___ $ ___ 3 `mappend` Product 9) -}
+                {- `shouldBe` 27 -}
+            {- (getProduct $ Product 3 `mappend` ___) -}
+                {- `shouldBe` 3 -}
+            {- (___ . ___ . ___ Product $ [3,4,2]) -}
+                {- `shouldBe` 24 -}
         it "and Sum newtypes" $ do
-            (getSum $ Sum 2 `mappend` Sum 9) `shouldBe` 11
-            (getSum $ mempty `mappend` Sum 3) `shouldBe` 3
-            (getSum . mconcat . map Sum $ [1,2,3]) `shouldBe` 6
+            pending
+            {- (___ $ ___ 2 `mappend` ___ 9) `shouldBe` 11 -}
+            {- (___ $ ___ `mappend` Sum 3) `shouldBe` 3 -}
+            {- (___ . ___ . ___ Sum $ [1,2,3]) `shouldBe` 6 -}
         it "has Any and All" $ do
-            (getAny $ Any True `mappend` Any False) `shouldBe` True
-            (getAll $ mempty `mappend` All True) `shouldBe` True
+            pending
+            {- (___ $ Any ___ `mappend` ___ False) `shouldBe` True -}
+            {- (getAll $ mempty `mappend` ___ ___) `shouldBe` True -}
