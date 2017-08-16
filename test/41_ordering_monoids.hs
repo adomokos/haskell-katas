4afbc8136dd7441e2702a4f338565ec919ce02e6
Attila Domokos <adomokos@gmail.com>
2017-08-16 17:03:02 -0500

    Add a verbose example

--- test/41_ordering_monoids.hs
+++ test/41_ordering_monoids.hs
@@ -3,6 +3,12 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 import Data.Monoid
 
+
+lengthCompareVerbose :: String -> String -> Ordering
+lengthCompareVerbose x y = let a = length x `compare` length y
+                               b = x `compare` y
+                           in if a == EQ then b else a
+
 lengthCompare :: String -> String -> Ordering
 lengthCompare x y = (length x `compare` length y) `mappend`
                     (x `compare` y)
@@ -16,6 +22,9 @@ lengthVowelCompare x y = (length x `compare` length y) `mappend`
 main :: IO ()
 main = hspec $ do
     describe "Monoids" $ do
+        it "verbose lengthcompare" $ do
+            lengthCompareVerbose "zen" "ants" `shouldBe` LT
+            lengthCompareVerbose "zen" "ant" `shouldBe` GT
         it "lengthcompare" $ do
             lengthCompare "zen" "ants" `shouldBe` LT
             lengthCompare "zen" "ant" `shouldBe` GT
