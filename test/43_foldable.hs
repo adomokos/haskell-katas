af6ebd6689c81926a0e1aaec00e9250bf9af9bd3
Attila Domokos <adomokos@gmail.com>
2017-08-20 14:34:29 -0500

    Prepare the foldable - monoid example

--- test/43_foldable.hs
+++ test/43_foldable.hs
@@ -4,14 +4,14 @@ import Control.Exception (evaluate)
 import Data.Monoid
 import qualified Data.Foldable as F
 
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+-- Define a tree Data Type
 
-instance F.Foldable Tree where
-    foldMap f Empty = mempty
-    foldMap f (Node x l r) = F.foldMap f l `mappend`
-                             f x `mappend`
-                             F.foldMap f r
+-- Add implementation for F.Foldable to Tree
+{- instance F.Foldable Tree where -}
+    {- ___ -}
 
+{-
+-- Create an sample tree sructure
 testTree = Node 5
             (Node 3
                 (Node 1 Empty Empty)
@@ -21,24 +21,33 @@ testTree = Node 5
                 (Node 8 Empty Empty)
                 (Node 10 Empty Empty)
             )
+-}
 
 main :: IO ()
 main = hspec $ do
     describe "Foldable" $ do
         it "Foldable foldr and Prelude Foldr are the same for lists" $ do
-            foldr (*) 1 [1,2,3] `shouldBe` 6
-            F.foldr (*) 1 [1,2,3] `shouldBe` 6
+            pending
+            {- foldr (___) 1 [1,2,3] `shouldBe` 6 -}
+            {- F.foldr (___) 1 [1,2,3] `shouldBe` 6 -}
         it "works with Maybe" $ do
-            F.foldl (+) 2 (Just 9) `shouldBe` 11
-            F.foldl (||) False (Just True) `shouldBe` True
+            pending
+            {- F.foldl (___) 2 (Just ___) `shouldBe` 11 -}
+            {- F.foldl (___) False (Just ___) `shouldBe` True -}
         it "can fold a tree with Foldable implementation" $ do
-            F.foldl (+) 0 testTree `shouldBe` 42
-            F.foldl (*) 1 testTree `shouldBe` 64800
+            pending
+            {- F.foldl ___ testTree `shouldBe` 42    -- sum -}
+            {- F.foldl ___ testTree `shouldBe` 64800 -- product -}
         it "can tell if the tree contains a single value" $ do
-            (getAny $ F.foldMap (\x -> Any $ x == 3) testTree)
-                `shouldBe` True
+            pending
+            {- Use Any to tell if 3 is in the tree or not -}
+            {- ___ -}
+                {- `shouldBe` True -}
         it "applies the function to all elements in the Tree" $ do
-            (getAny $ F.foldMap (\x -> Any $ x > 15) testTree)
-                `shouldBe` False
+            pending
+            {- Use Any to tell if any item is >15 in the Tree -}
+            {- ___ -}
+                {- `shouldBe` False -}
         it "can turn the Tree into a list" $ do
-            F.foldMap (\x -> [x]) testTree `shouldBe` [1,3,6,5,8,9,10]
+            pending
+            {- ___ testTree `shouldBe` [1,3,6,5,8,9,10] -}
