6cfce62d23ee3284edd4ab2189dae74262a7702a
Attila Domokos <adomokos@gmail.com>
2017-09-12 12:16:31 -0500

    Prepare the recursive data structures example for practice

--- test/30_recursive_data_structures.hs
+++ test/30_recursive_data_structures.hs
@@ -4,25 +4,15 @@ import Control.Exception (evaluate)
 
 {- Example 1 -}
 {- Define a list that uses Cons to add elements to the list -}
-infixr 5 :-:
-data List a = Empty | a :-: (List a) deriving (Show, Eq)
+{- Use infixr 5 -}
+{- data List a = ___ -}
 
 {- Define a Tree structure with EmptyTree and Node  -}
-data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Eq)
+{- data Tree a = ___ -}
 
-treeInsert :: (Ord a) => a -> Tree a -> Tree a
-treeInsert x EmptyTree = Node x EmptyTree EmptyTree
-treeInsert x (Node y left right)
-  | x == y = Node y left right
-  | x < y = Node y (treeInsert x left) right
-  | x > y = Node y left (treeInsert x right)
+{- treeInsert :: (Ord a) => a -> Tree a -> Tree a -}
 
-treeElem :: (Ord a) => a -> Tree a -> Bool
-treeElem x EmptyTree = False
-treeElem x (Node y left right)
-  | x == y = True
-  | x < y = treeElem x left
-  | x > y = treeElem x right
+{- treeElem :: (Ord a) => a -> Tree a -> Bool -}
 
 main :: IO()
 main = hspec $ do
@@ -33,10 +23,12 @@ main = hspec $ do
             {- 4 `Cons` (5 `Cons` Empty) -}
                 {- `shouldBe` Cons 4 (Cons 5 Empty) -}
         it "can set fixity declarations with infixr 5" $ do
-            5 :-: Empty `shouldBe` ((:-:) 5 Empty)
-            4 :-: 5 :-: Empty `shouldBe` ((:-:) 4 ((:-:) 5 Empty))
+            pending
+            {- 5 :-: Empty `shouldBe` ((:-:) 5 Empty) -}
+            {- 4 :-: 5 :-: Empty `shouldBe` ((:-:) 4 ((:-:) 5 Empty)) -}
         it "can express a binary search tree" $ do
-            let nums = [8,6,4,1,7,3,5]
-            let numsTree = foldr treeInsert EmptyTree nums
-            treeElem 5 numsTree `shouldBe` True
-            treeElem 2 numsTree `shouldBe` False
+            pending
+            {- let nums = [8,6,4,1,7,3,5] -}
+            {- let numsTree = foldr treeInsert EmptyTree nums -}
+            {- treeElem 5 numsTree `shouldBe` True -}
+            {- treeElem 2 numsTree `shouldBe` False -}
