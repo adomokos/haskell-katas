5e4be56b9b19b6cbb21f479ce0a46c6aea0c2ac8
Attila Domokos <adomokos@gmail.com>
2018-04-25 19:29:50 -0500

    Prepare the Phone Number cleaner spec

--- test/Katas/Exercises/PhoneNumberCleanerSpec.hs
+++ test/Katas/Exercises/PhoneNumberCleanerSpec.hs
@@ -6,11 +6,7 @@ main :: IO ()
 main = hspec spec
 
 convert :: [String] -> [(Int, String)]
-convert =
-    filterNon10s . makeTuples . map onlyNumericChars
-    where makeTuples pns = zipWith (\x y -> (y, x)) pns [0..]
-          onlyNumericChars pns = [x | x<-pns, x `elem` ['0'..'9']]
-          filterNon10s = filter (\(idx, pn) -> length pn == 10)
+convert _ = undefined
 
 spec :: Spec
 spec =
@@ -18,19 +14,23 @@ spec =
         let result = [(0,"3128342494")
                      ,(1,"2183422284")]
         it "can convert to a tuple with positions" $ do
+            pending
             let input = ["3128342494"
                         ,"2183422284"]
             convert input `shouldBe` result
         it "can filter out non-numeric chars" $ do
+            pending
             let input = ["a3128342494"
                         ,"218b3422284"]
             convert input `shouldBe` result
         it "can filter out every non-10 digit numbers" $ do
+            pending
             let input = ["3128342494"
                         ,"2183422284"
                         ,"123"]
             convert input `shouldBe` result
         it "can filter out invalid numbers mixed with other chars" $ do
+            pending
             let input = ["31283424"
                         ,"2183422284"
                         ,"12345678901"
