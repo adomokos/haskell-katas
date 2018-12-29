c72975d9b233e78b07f3ca576ee4b1117f5e6adf
Attila Domokos <adomokos@gmail.com>
2018-12-29 17:37:39 -0600

    Rename spec files with numbers in them

---
+++ test/Ex61_MonadsWriterTypeSpec.hs
@@ -0,0 +1,48 @@
+module Ex61_MonadsWriterTypeSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Control.Monad.Writer
+import Data.Monoid
+
+main :: IO ()
+main = hspec spec
+
+{-
+    newType Writer w a = Writer { runWriter :: (a, w) }
+    It's wrapped in a newtype so that it can be made an instance
+    of Monad and that its type is separate from a normal tuple.
+
+    instance (Monoid w) => Monad (Writer w) where
+        return x = Writer (x, mempty)
+        (Writer (x,v)) >>= f = let (Writer (y, v')) = f x in Writer (y, v `mappend` v'))
+-}
+
+-- Print "Got number: " and the var
+{- logNumber :: Int -> Writer [String] Int -}
+
+-- Multiply 3 and 5, but log the numbers using do notation
+{- multWithLog :: Writer [String] Int -}
+
+-- Same as above, but `tell` that "Gonna multiply these two"
+{- multWithLog' :: Writer [String] Int -}
+
+spec :: Spec
+spec = do
+    describe "Writer Type" $ do
+        it "can pair up runWriter with different monoids" $ do
+            pending
+            {- (runWriter (return 3 :: Writer String Int)) -}
+                {- `shouldBe` (3, ___) -}
+            {- (runWriter (return 3 :: Writer (Sum Int) Int)) -}
+                {- `shouldBe` (3, ___) -}
+            {- (runWriter (return 3 :: Writer (Product Int) Int)) -}
+                {- `shouldBe` (3, ___) -}
+        it "can use do notation as well" $ do
+            pending
+            {- runWriter multWithLog -}
+                {- `shouldBe` (15, ["Got number: 3", "Got number: 5"]) -}
+        it "can 'tell' what's going on" $ do
+            pending
+            {- runWriter multWithLog' -}
+                {- `shouldBe` (15, ["Got number: 3", "Got number: 5","Gonna multiply these two"]) -}
---
+++ test/Ex62_MonadsLoggingWithWriterSpec.hs
@@ -0,0 +1,49 @@
+module Ex62_MonadsLoggingWithWriterSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Control.Monad.Writer
+import Data.Monoid
+
+main :: IO ()
+main = hspec spec
+
+-- Greatest common deviser
+{- gcd' :: Int -> Int -> Int -}
+
+{- gcdWithLog :: Int -> Int -> Writer [String] Int -}
+
+{- newtype DiffList a = DiffList { getDiffList :: [a] -> [a] } -}
+
+{- toDiffList :: [a] -> DiffList a -}
+
+{- fromDiffList :: DiffList a -> [a] -}
+
+{- instance Monoid (DiffList a) where -}
+
+{- gcdWithDiffList :: Int -> Int -> Writer (DiffList String) Int -}
+
+spec :: Spec
+spec = do
+    describe "Logging with Writer" $ do
+        it "can find the greatest common devisor" $ do
+            pending
+            {- gcd' 8 3 `shouldBe` 1 -}
+        it "can decorate the gcd function with logging" $ do
+            pending
+            {- (fst $ runWriter $ gcdWithLog 8 3) `shouldBe` 1 -}
+            {- (snd $ runWriter $ gcdWithLog 8 3) -}
+                {- `shouldBe` ["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"] -}
+        it "can efficiently append to difference list" $ do
+            pending
+            {- (fromDiffList (toDiffList [1,2,3,4] `mappend` toDiffList [1,2,3])) -}
+                {- `shouldBe` [1,2,3,4,1,2,3] -}
+        it "can log with DiffList String" $ do
+            pending
+            {- (fromDiffList . snd . runWriter $ gcdWithDiffList 8 3) -}
+                {- `shouldBe` ["8 mod 3 = 2","3 mod 2 = 1","2 mod 1 = 0","Finished with 1"] -}
+
+{-
+    To run it in the RePL:
+    ghci> mapM_ putStrLn . fromDiffList . snd . runWriter $ gcdWithDiffList 110 34
+-}
