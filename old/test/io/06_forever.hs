ff8cd754e3ad958b4a2e2d8d55d7fddfb8c4c808
Attila Domokos <adomokos@gmail.com>
2019-10-01 20:13:05 -0500

    Remove old directory

--- old/test/io/06_forever.hs
+++
@@ -1,9 +0,0 @@
-{-
-    forever from Control.Monad will repeat the IO action forever
-    1. Read in line
-    2. Make it upper cased
--}
-import Control.Monad
-import Data.Char
-
-main = forever ___
